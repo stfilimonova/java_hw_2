dacab99a3967d2ccd6bc8924e8cb7db8208ce2e3
stfilimonova <stfilimonova@mail.ru>
2023-03-14 05:27:48 +0300

    typo in main file fixed

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         double ticketCost = 8150;
         double sumForMile = 20;
 
-        System.out.println("Стооимость билета: " + ticketCost);
+        System.out.println("Стоимость билета: " + ticketCost);
         System.out.println("Количество начисленных миль: " + (int)(ticketCost / sumForMile));
     }
 }
